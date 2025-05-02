f31070f167885fbb308ff84d418bc50985cacedc
Neal_Eleazar_J_Ramos <nealramos72@gmail.com>
2025-05-02 21:43:17 +0800

    update the sql schema

--- edugrant_database.sql
+++ edugrant_database.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 26, 2025 at 06:21 PM
+-- Generation Time: May 02, 2025 at 03:42 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -24,14 +24,21 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `admin_account`
+-- Table structure for table `admin_accounts`
 --
 
-CREATE TABLE `admin_account` (
+CREATE TABLE `admin_accounts` (
   `adminId` int(255) NOT NULL,
   `adminEmail` varchar(255) DEFAULT NULL,
   `adminPassword` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `admin_accounts`
+--
+
+INSERT INTO `admin_accounts` (`adminId`, `adminEmail`, `adminPassword`) VALUES
+(1, 'nealadmin@gmail.com', 'nealadmin');
 
 -- --------------------------------------------------------
 
@@ -40,7 +47,11 @@ CREATE TABLE `admin_account` (
 --
 
 CREATE TABLE `announcements` (
-  `announcementId` int(255) NOT NULL
+  `announcementId` int(255) NOT NULL,
+  `announcementTitle` varchar(255) NOT NULL,
+  `announcementDate` date NOT NULL,
+  `announcementFile` varchar(255) NOT NULL,
+  `announcementContent` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -50,8 +61,23 @@ CREATE TABLE `announcements` (
 --
 
 CREATE TABLE `scholarships` (
-  `scholarshipId` int(255) NOT NULL
+  `scholarshipId` int(255) NOT NULL,
+  `scholarshipName` varchar(255) NOT NULL,
+  `scholarshipDealine` varchar(255) NOT NULL,
+  `scholarshipLogo` varchar(255) NOT NULL,
+  `scholarshipCover` varchar(255) NOT NULL,
+  `scholarshipDescription` varchar(255) NOT NULL,
+  `scholarshipDocuments` varchar(255) NOT NULL,
+  `totalApplicants` int(255) NOT NULL DEFAULT 0,
+  `totalApproved` int(255) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `scholarships`
+--
+
+INSERT INTO `scholarships` (`scholarshipId`, `scholarshipName`, `scholarshipDealine`, `scholarshipLogo`, `scholarshipCover`, `scholarshipDescription`, `scholarshipDocuments`, `totalApplicants`, `totalApproved`) VALUES
+(17, 'Win Gachalian', '2025-05-13', 'https://tlisrihwtsvpkvbsohcj.supabase.co/storage/v1/object/public/scholarship-files/sponsor-1746191419439-b551b67f-df77-4d57-959f-7ee9757bd417.jpg', 'https://tlisrihwtsvpkvbsohcj.supabase.co/storage/v1/object/public/scholarship-files/cover-1746191419439-c47a634e-223c-4296-8f57-b8b1ebf37bc5.jpg', 'hahahahaha', '[{\"label\":\"COR\"}]', 0, 0);
 
 -- --------------------------------------------------------
 
@@ -60,19 +86,15 @@ CREATE TABLE `scholarships` (
 --
 
 CREATE TABLE `scholarships_archive` (
-  `archiveId` int(255) NOT NULL,
-  `scholarshipId` int(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
--- Table structure for table `scholarships_reports`
---
-
-CREATE TABLE `scholarships_reports` (
-  `reportId` int(255) NOT NULL,
-  `userId` int(255) DEFAULT NULL
+  `scholarshipId` int(255) DEFAULT NULL,
+  `scholarshipName` varchar(255) DEFAULT NULL,
+  `scholarshipDeadline` varchar(255) DEFAULT NULL,
+  `scholarshipLogo` varchar(255) DEFAULT NULL,
+  `scholarshipCover` varchar(255) DEFAULT NULL,
+  `scholarshipDescription` varchar(255) DEFAULT NULL,
+  `scholarshipDocuments` varchar(255) DEFAULT NULL,
+  `scholarshipApplicants` int(255) DEFAULT NULL,
+  `scholarshipApproved` int(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
@@ -124,7 +146,48 @@ INSERT INTO `user_account` (`userID`, `firstName`, `middleName`, `lastName`, `us
 CREATE TABLE `user_applications` (
   `applicationId` int(255) NOT NULL,
   `userId` int(255) DEFAULT NULL,
-  `scholarshipId` int(255) DEFAULT NULL
+  `scholarshipId` int(255) DEFAULT NULL,
+  `status` varchar(255) NOT NULL DEFAULT 'pending'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_applications_approved`
+--
+
+CREATE TABLE `user_applications_approved` (
+  `applicationId` int(255) DEFAULT NULL,
+  `userId` int(255) DEFAULT NULL,
+  `scholarshipId` int(255) DEFAULT NULL,
+  `status` varchar(255) NOT NULL DEFAULT 'approved'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_applications_missing`
+--
+
+CREATE TABLE `user_applications_missing` (
+  `applicationId` int(255) DEFAULT NULL,
+  `userId` int(255) DEFAULT NULL,
+  `scholarshipId` int(255) DEFAULT NULL,
+  `status` varchar(255) NOT NULL DEFAULT 'missing',
+  `missingFiles` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_applications_rejected`
+--
+
+CREATE TABLE `user_applications_rejected` (
+  `applicationId` int(255) DEFAULT NULL,
+  `userId` int(255) DEFAULT NULL,
+  `scholarshipId` int(255) DEFAULT NULL,
+  `status` varchar(255) NOT NULL DEFAULT 'rejected'
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
@@ -132,9 +195,9 @@ CREATE TABLE `user_applications` (
 --
 
 --
--- Indexes for table `admin_account`
+-- Indexes for table `admin_accounts`
 --
-ALTER TABLE `admin_account`
+ALTER TABLE `admin_accounts`
   ADD PRIMARY KEY (`adminId`);
 
 --
@@ -153,15 +216,7 @@ ALTER TABLE `scholarships`
 -- Indexes for table `scholarships_archive`
 --
 ALTER TABLE `scholarships_archive`
-  ADD PRIMARY KEY (`archiveId`),
   ADD KEY `scholarshipId` (`scholarshipId`);
-
---
--- Indexes for table `scholarships_reports`
---
-ALTER TABLE `scholarships_reports`
-  ADD PRIMARY KEY (`reportId`),
-  ADD KEY `userId` (`userId`);
 
 --
 -- Indexes for table `security_code`
@@ -184,14 +239,32 @@ ALTER TABLE `user_applications`
   ADD KEY `scholarshipId` (`scholarshipId`);
 
 --
+-- Indexes for table `user_applications_approved`
+--
+ALTER TABLE `user_applications_approved`
+  ADD KEY `applicationId` (`applicationId`);
+
+--
+-- Indexes for table `user_applications_missing`
+--
+ALTER TABLE `user_applications_missing`
+  ADD KEY `applicationId` (`applicationId`);
+
+--
+-- Indexes for table `user_applications_rejected`
+--
+ALTER TABLE `user_applications_rejected`
+  ADD KEY `applicationId` (`applicationId`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `admin_account`
+-- AUTO_INCREMENT for table `admin_accounts`
 --
-ALTER TABLE `admin_account`
-  MODIFY `adminId` int(255) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `admin_accounts`
+  MODIFY `adminId` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `announcements`
@@ -203,25 +276,13 @@ ALTER TABLE `announcements`
 -- AUTO_INCREMENT for table `scholarships`
 --
 ALTER TABLE `scholarships`
-  MODIFY `scholarshipId` int(255) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `scholarships_archive`
---
-ALTER TABLE `scholarships_archive`
-  MODIFY `archiveId` int(255) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT for table `scholarships_reports`
---
-ALTER TABLE `scholarships_reports`
-  MODIFY `reportId` int(255) NOT NULL AUTO_INCREMENT;
+  MODIFY `scholarshipId` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT for table `security_code`
 --
 ALTER TABLE `security_code`
-  MODIFY `codeId` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=69;
+  MODIFY `codeId` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=98;
 
 --
 -- AUTO_INCREMENT for table `user_account`
@@ -246,17 +307,29 @@ ALTER TABLE `scholarships_archive`
   ADD CONSTRAINT `scholarships_archive_ibfk_1` FOREIGN KEY (`scholarshipId`) REFERENCES `scholarships` (`scholarshipId`);
 
 --
--- Constraints for table `scholarships_reports`
---
-ALTER TABLE `scholarships_reports`
-  ADD CONSTRAINT `scholarships_reports_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `user_account` (`userID`);
-
---
 -- Constraints for table `user_applications`
 --
 ALTER TABLE `user_applications`
   ADD CONSTRAINT `user_applications_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `user_account` (`userID`),
   ADD CONSTRAINT `user_applications_ibfk_2` FOREIGN KEY (`scholarshipId`) REFERENCES `scholarships` (`scholarshipId`);
+
+--
+-- Constraints for table `user_applications_approved`
+--
+ALTER TABLE `user_applications_approved`
+  ADD CONSTRAINT `user_applications_approved_ibfk_1` FOREIGN KEY (`applicationId`) REFERENCES `user_applications` (`applicationId`);
+
+--
+-- Constraints for table `user_applications_missing`
+--
+ALTER TABLE `user_applications_missing`
+  ADD CONSTRAINT `user_applications_missing_ibfk_1` FOREIGN KEY (`applicationId`) REFERENCES `user_applications` (`applicationId`);
+
+--
+-- Constraints for table `user_applications_rejected`
+--
+ALTER TABLE `user_applications_rejected`
+  ADD CONSTRAINT `user_applications_rejected_ibfk_1` FOREIGN KEY (`applicationId`) REFERENCES `user_applications` (`applicationId`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
